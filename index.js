50613008e4a9877f242fa739db444f24a41f118f
Joe Yang <jose.ozelzo10@gmail.com>
2020-03-31 18:57:35 -0300

    Corrigindo um teste que fiz e esqueci de consertar

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 
     const express = require('express');
 
-    const app = express();
-    const amor = function(){
-        while (true){
-            return "Eu te amo!"}}
-            
+    const app = express();            
 
     app.get("/", (request, response) => {
-        return response.send(amor);
+        return response.json({
+            evento: "Semana OmniStack",
+            aluno: "José Carlos da Silva Filho",
+            matricula: 2017102009,
+            turma: 823,
+            unidade: "Méier II"
+        });
     });
 
     app.listen(3333);
